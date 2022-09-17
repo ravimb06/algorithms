ab65f960cf79f1470f7fec4f73d9a2e9b1e478d9
Ali Bogatyrev <marksheyder06@gmail.com>
2022-09-17 13:43:19 +0300

    added task "a"

---
+++ my_tasks/a.py
@@ -0,0 +1,15 @@
+from typing import Tuple
+
+
+def get_sum(a: int, b: int) -> int:
+    return a + b
+
+
+def read_input() -> Tuple[int, int]:
+    a = int(input())
+    b = int(input())
+    return a, b
+
+
+a, b = read_input()
+print(get_sum(a, b))
