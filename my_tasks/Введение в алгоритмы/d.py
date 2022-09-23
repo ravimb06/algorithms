f240bdad6ef2006045eeba00efeaf07d8f375c12
Ali Bogatyrev <marksheyder06@gmail.com>
2022-09-23 21:27:36 +0300

    added task "C"

--- my_tasks/Введение в алгоритмы/d.py
+++ my_tasks/Введение в алгоритмы/d.py
@@ -1,12 +1,19 @@
 def main():
-    len_text = int(input())
-    text = list(input().split())
-    max_word = text[0]
-    for i in range(len(text)):
-        if len(text[i]) > len(max_word):
-            max_word = text[i]
-    print(max_word)
-    print(len(max_word))
+    amount_days = int(input())
+    temperatures = list(map(int, input().split()))
+    counter = 0
+    if amount_days > 1:
+        for i in range(len(temperatures)):
+            if i > 0 and i < len(temperatures) - 1:
+                if temperatures[i] > temperatures[i-1] and temperatures[i] > temperatures[i+1]:
+                    counter += 1
+            if i == 0 and temperatures[i] > temperatures[i+1]:
+                counter += 1
+            if i == len(temperatures) - 1 and temperatures[i] > temperatures[i-1]:
+                counter += 1
+    if amount_days == 1:
+        counter += 1
+    print(counter)
 
 if __name__ == '__main__':
     main()
