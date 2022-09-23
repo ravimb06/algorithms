1c19f69ed9eec8afbe7c5e734620dda9422511ef
Ali Bogatyrev <marksheyder06@gmail.com>
2022-09-23 22:11:07 +0300

    added task "F"

---
+++ my_tasks/Введение в алгоритмы/f.py
@@ -0,0 +1,16 @@
+import re
+
+def main():
+    inputed_text = input().lower()
+    reg = re.compile('[^a-zA-Z]')
+    text = reg.sub('', inputed_text)
+    reversed_text = text[::-1]
+    is_palindrome = True
+    for i in range(len(text)):
+        if text[i] != reversed_text[i]:
+            is_palindrome = False
+            break
+    print(is_palindrome)
+
+if __name__ == '__main__':
+    main()
