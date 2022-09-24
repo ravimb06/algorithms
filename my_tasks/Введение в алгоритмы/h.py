8be8945a8f9a27c7a38137f24beba345f972f0bc
Ali Bogatyrev <marksheyder06@gmail.com>
2022-09-24 14:47:05 +0300

    added task "H"

---
+++ my_tasks/Введение в алгоритмы/h.py
@@ -0,0 +1,8 @@
+def main():
+    digit_1 = int(input(), 2)
+    digit_2 = int(input(), 2)
+    digit_sum = format(digit_1 + digit_2, 'b')
+    print(digit_sum)
+
+if __name__ == '__main__':
+    main()
