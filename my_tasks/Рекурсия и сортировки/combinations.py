78deb738ed8aa746185a8076168944e1394672d2
Ali Bogatyrev <marksheyder06@gmail.com>
2022-11-08 18:39:58 +0300

    solved task "Combinations"

---
+++ my_tasks/Рекурсия и сортировки/combinations.py
@@ -0,0 +1,20 @@
+def letterCombinations(digits):
+    letters = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+               '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+
+    def backtrack(digits, path, res):
+        if digits == '':
+            res.append(path)
+            return
+        for letter in letters[digits[0]]:
+            path += letter
+            backtrack(digits[1:], path, res)
+            path = path[:-1]
+    res = []
+    backtrack(digits, '', res)
+    for x in res:
+        print(x, end=' ')
+
+
+vvod = (input())
+letterCombinations(vvod)
